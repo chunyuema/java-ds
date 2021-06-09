5dd5afba8bc841dfc0dac3e06e97e69e2f7b804d
Chunyue Ma <cma01@wesleyan.edu>
2021-06-09 00:17:40 -0400

    stack(implemented using LinkedList)

---
+++ Stack.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.EmptyStackException;
+
+public class Stack<T> implements Iterable<T> {
+    private LinkedList<T> list = new LinkedList<>();
+
+    public Stack() {
+    }
+
+    public Stack(T firstElem) {
+        push(firstElem);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void push(T elem) {
+        list.addLast(elem);
+    }
+
+    public T pop() {
+        if (isEmpty())
+            throw new EmptyStackException();
+        return list.removeLast();
+    }
+
+    public T peek() {
+        if (isEmpty())
+            return null;
+    }
+}
